1d8eb500b47e800e99a9da99a1b63855fc075581
chavimiller <chaviweisman@gmail.com>
2025-11-19 11:11:53 +0200

    Add express validator

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 
+const { body, validationResult } = require("express-validator");
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
